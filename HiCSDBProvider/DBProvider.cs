d3eb8fffd7e10104b81afc626c2465077999f702
XuminRong <xumingxsh21@126.com>
2016-06-03 16:57:55 +0800

    Signed-off-by: XuminRong <xumingxsh21@126.com> 添加HiCSDBProvider 数据提供者有很多,此处只提供以数据库为数据源,以XML为SQL存储位置的数据提供者 注: 此处未考虑多数据库问题,以后会根据实际情况提供

---
+++ HiCSDBProvider/DBProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+using HiCSDB;
+
+namespace HiCSDBProvider
+{
+    /// <summary>
+    /// 数据提供者有很多,此处只提供以数据库为数据源,以XML为SQL存储位置的数据提供者
+    /// 注: 此处未考虑多数据库问题,以后会根据实际情况提供
+    /// XuminRong 2016-06-03
+    /// </summary>
+    public class DBProvider
+    {
+        static DBOperate db = null;
+
+        static DBOperate DB
+        {
+            get
+            {
+                if (db == null)
+                {
+                    db = new DBOperate(ProvidConfig.Conn, ProvidConfig.DBType);
+                }
+                return db;
+            }
+        }
+    }
+}
